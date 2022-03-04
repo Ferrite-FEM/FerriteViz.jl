ec3427106ae77b7741b3141702e7bf16e17895e3
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2022-03-04 14:38:18 +0100

    add options

--- src/plotsplotting.jl
+++ src/plotsplotting.jl
@@ -1,16 +1,43 @@
-function solutionplot_pgfplots(plotter,args...;kwargs...)
+function solutionplot_pgfplots(plotter;
+                               axis_options=@pgf{},
+                               plot_options=@pgf{"shader" = "faceted"})
     solution = reshape(transfer_solution(plotter,plotter.u[]; field_idx=1, process=norm), num_vertices(plotter))
     u_matrix = transfer_solution(plotter,plotter.u[]; field_idx=1, process=identity)
     coords = plotter.physical_coords .+ u_matrix
-    @pgf PGFPlotsX.Axis({
-        "scale mode" = "scale uniformly",
-    },
+    @pgf PGFPlotsX.Axis({axis_options..., "scale mode" = "scale uniformly"},
     Plot3(
     {
+        plot_options...,
         patch,
+        "axis equal image",
         "table/row sep" = "\\\\",
-        "shader" = "flat",
-        patch_table = TableData(plotter.triangles .- 1)
+        patch_table = TableData(plotter.triangles .- 1),
+    },
+    Table(
+        {
+            point_meta = raw"\thisrow{c}"
+        },
+        :x => coords[:,1],
+        :y => coords[:,2],
+        :z => coords[:,3],
+        :c => solution)))
+end
+
+function cellplot_pgfplots(plotter,celldata;
+                           axis_options=@pgf{},
+                           plot_options=@pgf{"shader" = "faceted"})
+    solution = reshape(transfer_solution(plotter,plotter.u[]; field_idx=1, process=norm), num_vertices(plotter))
+    u_matrix = transfer_solution(plotter,plotter.u[]; field_idx=1, process=identity)
+    solution =  reshape(transfer_scalar_celldata(plotter, celldata; process=identity), num_vertices(plotter))
+    coords = plotter.physical_coords .+ u_matrix
+    @pgf PGFPlotsX.Axis({axis_options...,"scale mode" = "scale uniformly"},
+    Plot3(
+    {
+        plot_options...,
+        patch,
+        "axis equal image",
+        "table/row sep" = "\\\\",
+        patch_table = TableData(plotter.triangles .- 1),
     },
     Table(
         {

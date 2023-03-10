664960ea07ed4e8a632d3073f3f06a98c50345e5
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2023-03-10 15:32:33 +0100

    fix wrong dofs

--- src/utils.jl
+++ src/utils.jl
@@ -349,7 +349,7 @@ function transfer_solution(plotter::MakiePlotter{dim,DH,T}, u::Vector; field_idx
     localbuffer = zeros(T,field_dim)
     _local_coords = Ferrite.getcoordinates(grid,1)
     _local_celldofs = Ferrite.celldofs(dh,1)
-    _celldofs_field = @view _local_celldofs[local_dof_range]
+    _celldofs_field = reshape(_local_celldofs[local_dof_range], (field_dim, Ferrite.getnbasefunctions(ip_field)))
     _local_ref_coords = Tensors.Vec{dim}(ref_coords[1,:])
 
     for (isvisible,(cell_idx,cell_geo)) in zip(plotter.visible,enumerate(Ferrite.getcells(dh.grid)))
@@ -365,14 +365,14 @@ function transfer_solution(plotter::MakiePlotter{dim,DH,T}, u::Vector; field_idx
         #end
         Ferrite.getcoordinates!(_local_coords,grid,cell_idx)
         Ferrite.celldofs!(_local_celldofs,dh,cell_idx)
-        _celldofs_field = @view _local_celldofs[local_dof_range]
+        _celldofs_field = reshape(@view(_local_celldofs[local_dof_range]), (field_dim, Ferrite.getnbasefunctions(ip_field)))
         ncvertices = ntriangles(cell_geo)*n_vertices_per_tri
         # TODO replace this with a triangle-to-cell map.
         for i in 1:ncvertices
             _local_ref_coords = Tensors.Vec{dim}(@view(ref_coords[current_vertex_index,:]))
             Ferrite.reinit!(pv, _local_coords, _local_ref_coords)
-            for d in 1:_processreturn
-                val_buffer[d] = Ferrite.function_value(pv, 1, @view(u[_celldofs_field][d:field_dim:((n_basefuncs*field_dim)-(field_dim-d))]))
+            for d in 1:field_dim
+                val_buffer[d] = Ferrite.function_value(pv, 1, @view(u[_celldofs_field[d,:]]))
             end
             val = process(val_buffer)
             for d in 1:_processreturn

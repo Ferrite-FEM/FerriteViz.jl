a5618223b3501b406802d285be15be2ce33262fe
Adarsh Palaskar <83298237+adarshpalaskar1@users.noreply.github.com>
2024-04-11 02:11:19 +0530

    Update src/makieplotting.jl
    Co-authored-by: Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>

--- src/makieplotting.jl
+++ src/makieplotting.jl
@@ -312,7 +312,7 @@ values are transformed to a scalar based on `process` which defaults to the magn
 - `scale_plot = false`
 - `shading = false`
 - `colormap = :cividis`
-- `colorrange = (0,1)`
+- `colorrange=Makie.automatic`
 """
 @recipe(Surface) do scene
     Attributes(
